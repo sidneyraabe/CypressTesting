9b5e733f9fdd3195426b6f5e6d93752413dd2c7e
Sidney Raabe <sidneyraabe@gmail.com>
2022-05-13 15:21:45 -0400

    First draft of tests

---
+++ cypress/integration/sraabe-tests/product-page-testing.spec.js
@@ -0,0 +1,23 @@
+describe('ProductPageTesting', () => {
+    beforeEach(() => {
+        cy.visit('https://bubbling-summit-zcgdxlhsvi1d.vapor-farm-e1.com/');
+    })
+
+    it('Has visible product title', () => {
+        cy.contains('Everyday Ruck Snack').should('be.visible');
+    })
+
+    it('Has visible "Add to bag" button', () => {
+        cy.get('button').contains('Add to bag').should('be.visible');
+    })
+
+    it('Has visible reviews section', () => {
+        cy.get('#reviews-heading').contains('Customer Reviews').should('be.visible');
+    })
+
+    it('Has a visible featured review', () => {
+        cy.get('.sr-only').contains('Recent reviews');     
+    })
+});
+
+//npm run cytest
